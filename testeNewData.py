9684936c25dbe32bf0fcf9be06fb1826995b8990
ProfHeydeFranca <125465761+ProfHeydeFranca@users.noreply.github.com>
2024-02-13 10:33:42 +0100

    Update testeNewData.py

--- testeNewData.py
+++ testeNewData.py
@@ -25,36 +25,40 @@ dados_bacterias = pd.get_dummies(df)
 lista_de_dados = []
 
 print("Read df_salt")
-
 dados_genomas = pd.read_csv('/work/no58rok/bacterial_phenotypes_draco/df_features_objects/df_features_halophily_pipeline2.2_merged.csv')
+#Define 'Best assembly' as index of dataframe
+dados_bacterias['Best assembly'] = dados_bacterias.index
+dados_genomas['Best assembly'] = dados_genomas.index
 
+#Merge data with exact match
+dados_bacterias_merged = dados_bacteriass.merge(dados_genomas, left_index=True, right_index=True, how='outer')
 
-print("Read df_features")
+#print("Read df_features")
 # Iterar sobre as linhas do bloco de dados do segundo arquivo
-for indice, linha in dados_genomas_chunk.iterrows():
+#for indice, linha in dados_genomas_chunk.iterrows():
         # Verificar se 'Best assembly' coincide com algum valor do primeiro arquivo
-        if linha['Unnamed: 0'] in dados_bacterias['Best assembly'].values:
+ #       if linha['Unnamed: 0'] in dados_bacterias['Best assembly'].values:
             # Obter a linha correspondente do primeiro arquivo
-            linha_bacteria = dados_bacterias[dados_bacterias['Best assembly'] == linha['Best assembly']].iloc[0]
+  #          linha_bacteria = dados_bacterias[dados_bacterias['Best assembly'] == linha['Best assembly']].iloc[0]
 
             # Copiar todas as colunas do segundo arquivo
-            dados_completos = linha.to_dict()
+#            dados_completos = linha.to_dict()
 
             # Adicionar colunas específicas do primeiro arquivo
-            dados_completos.update({
-                'Class': linha_bacteria['Class'],
-                'Species': linha_bacteria['Species'],
-                'Salt optimum': linha_bacteria['Salt optimum'],
-                'Salt all': linha_bacteria['Salt all']
-            })
+ #           dados_completos.update({
+  #              'Class': linha_bacteria['Class'],
+   #             'Species': linha_bacteria['Species'],
+    #            'Salt optimum': linha_bacteria['Salt optimum'],
+     #           'Salt all': linha_bacteria['Salt all']
+      #      })
 
-            # Adicionar dados completos à lista de dados
-            lista_de_dados.append(dados_completos)
+       #     # Adicionar dados completos à lista de dados
+        #    lista_de_dados.append(dados_completos)
 
 # Converter lista de dados para DataFrame
-df_dados_final = pd.DataFrame(lista_de_dados)
+#df_dados_final = pd.DataFrame(lista_de_dados)
 
 # Salvar o DataFrame em um novo arquivo CSV
-df_dados_final.to_csv('dados_bacterias_com_genomas.csv', index=False)
+dados_bacterias_merged.to_csv('dados_bacterias_com_genomas_merge.csv', index=False)
 
 print("Dados salvos com sucesso!")
