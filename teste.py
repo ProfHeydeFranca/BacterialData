4aca12ec6f727932396ae736337371f626136b84
ProfHeydeFranca <125465761+ProfHeydeFranca@users.noreply.github.com>
2024-02-15 12:25:48 +0100

    Update teste.py
    merge

--- teste.py
+++ teste.py
@@ -1,9 +1,7 @@
 import pandas as pd
-#from tqdm import tqdm  # Importar tqdm para a barra de progresso
-
-#Linhas sugeridas ############# INICIO
-
-#EXACT MATCH OPTION
+#Reading the data
+df_features = pd.read_csv('work/groups/VEO/shared_data/bia_heyde/df_features_halophily_pipeline2.2_merged.csv')
+df_salt = pd.read_csv('work/groups/VEO/shared_data/bia_heyde/df_salt_filtered-salinity_best_assembly_temp_pH_oxygen_merged_assemblies.csv')
 
 #Define 'Best assembly' as index of dataframe
 df_features['Best assembly'] = df_features.index
@@ -12,67 +10,8 @@ df_salt['Best assembly'] = df_salt.index
 #Merge data with exact match
 df_features = df_features.merge(df_salt, left_index=True, right_index=True, how='outer')
 
-#Linhas sugeridas ############# FIM
-
-# Ler apenas a primeira linha do arquivo CSV
-#primeira_linha = pd.read_csv('/work/no58rok/bacterial_phenotypes_draco/df_features_objects/df_features_halophily_pipeline2.2_merged.csv', nrows=2)
-
-# Imprimir a primeira linha
-#print(primeira_linha)
-
-######
-# Carregar dados do primeiro arquivo CSV (bactérias)
-dados_bacterias = pd.read_csv('/work/no58rok/bacterial_phenotypes_draco/df_salt_objects/df_salt_filtered-salinity_best_assembly_temp_pH_oxygen_merged_assemblies.csv')
-#print(dados_bacterias.columns)
-
-print("Read df_salt")
-
-dados_genomas = pd.read_csv('/work/no58rok/bacterial_phenotypes_draco/df_features_objects/df_features_halophily_pipeline2.2_merged.csv')
-
-
-print("Read df_features")
-
-# Criar lista de dados
-lista_de_dados = []
-
-# Definir tamanho do bloco para leitura
-#chunksize = 100000
-
-# Ler o arquivo CSV em blocos menores
-#for dados_genomas_chunk in pd.read_csv('/work/no58rok/bacterial_phenotypes_draco/df_features_objects/df_features_halophily_pipeline2.2_merged.csv', chunksize=chunksize):
-# Iterar sobre as linhas do bloco de dados do segundo arquivo
-for indice, linha in dados_genomas.iterrows():
-    # Verificar se 'Best assembly' coincide com algum valor do primeiro arquivo
-
-    #Debug
-    #print(indice, linha)
-
-    if linha['Unnamed: 0'] in dados_bacterias['Best assembly'].values:
-        # Obter a linha correspondente do primeiro arquivo
-        linha_bacteria = dados_bacterias[dados_bacterias['Best assembly'] == linha['Best assembly']].iloc[0]
-
-        #Debug
-        #print(linha_bacteria)
-
-        # Copiar todas as colunas do segundo arquivo
-        dados_completos = linha.to_dict()
-
-        # Adicionar colunas específicas do primeiro arquivo
-        dados_completos.update({
-            'Class': linha_bacteria['Class'],
-            'Species': linha_bacteria['Species'],
-            'Salt optimum': linha_bacteria['Salt optimum'],
-            'Salt all': linha_bacteria['Salt all']
-        })
-
-        # Adicionar dados completos à lista de dados
-        lista_de_dados.append(dados_completos)
-
-# Converter lista de dados para DataFrame
-df_dados_final = pd.DataFrame(lista_de_dados)
-
 # Salvar o DataFrame em um novo arquivo CSV
-df_dados_final.to_csv('dados_bacterias_com_genomas.csv', index=False)
+df_features.to_csv('dados_bacterias_com_genomas.csv', index=False)
 
 print("Dados salvos com sucesso!")
 
